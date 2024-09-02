ccba67e12d30018bc91d42f64c0f61adbb7368e2
Bart <bart.vanderwal@han.nl>
2024-09-02 23:33:44 +0200

    Fixed #1 - Werkt nu voor -1, 0 en 1, rest\generiek TODO.

--- src/main/java/nl/han/devops/PriemTester.java
+++ src/main/java/nl/han/devops/PriemTester.java
@@ -1,8 +1,11 @@
 package nl.han.devops;
 
 public class PriemTester {
-    public boolean isPriemgetal(int candidate) {
-        throw new UnsupportedOperationException("Nog niet geïmplementeerd.");
+    public boolean isPriemgetal(int kandidaat) {
+        if (kandidaat < 2) {
+            return false;
+        }
+        throw new UnsupportedOperationException("Niet geheel geïmplementeerd.");
     }
 
 }
---
+++ src/test/java/nl/han/devops/PriemTesterTest.java
@@ -0,0 +1,38 @@
+package nl.han.devops;
+
+import org.junit.jupiter.api.*;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class PriemTesterTest {
+
+	private final PriemTester priemTester = new PriemTester();
+
+	@ParameterizedTest
+	@ValueSource(ints = {-1, 0, 1})
+	public void isPriemgetal_WaardenKleinerDan2_ReturnFalse(int value) {
+		boolean result = priemTester.isPriemgetal(value);
+
+		Assertions.assertFalse(result, value + " zou geen priemgetal moeten zijn.");
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = {4, 6, 8, 9, 10, 12})
+	public void isPriemgetal_onwaarden_voor_niet_priemgetallen(int value) {
+		boolean result = priemTester.isPriemgetal(value);
+
+		Assertions.assertFalse(result, value + " zou geen priemgetal moeten zijn.");
+	}
+	@ParameterizedTest
+	@ValueSource(ints = {2, 3, 5, 7, 9, 11})
+	public void isPriemgetal_waar_voor_priemgetallen(int value) {
+		boolean result = priemTester.isPriemgetal(value);
+
+		Assertions.assertTrue(result, value + " zou een priemgetal moeten zijn.");
+	}
+
+}
