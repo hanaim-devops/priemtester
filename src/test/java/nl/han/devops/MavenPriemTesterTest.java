78c26369c03bf2bd5cbf0f23d62544b7f4049e32
Bart <bart.vanderwal@han.nl>
2024-10-31 14:42:48 +0100

    Oeps, test klasse was nog genest in andere klasse; copy-paste fout, dus MavenPriemTester werd niet gerund. Nu wel.

--- src/test/java/nl/han/devops/MavenPriemTesterTest.java
+++ src/test/java/nl/han/devops/MavenPriemTesterTest.java
@@ -1,56 +1,53 @@
 package nl.han.devops;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class MavenPriemTesterTest {
 
-    @SpringBootTest
-    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-    public class CustomPriemTesterTest {
+    private final PriemTester priemTester = new MavenPriemTester();
 
-        private final PriemTester priemTester = new MavenPriemTester();
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, 1})
+    public void isPriemgetal_waar_voor_getallen_kleiner_dan_twee(int value) {
+        boolean result = priemTester.isPriemgetal(value);
 
-        @ParameterizedTest
-        @ValueSource(ints = {-1, 0, 1})
-        public void isPriemgetal_waar_voor_getallen_kleiner_dan_twee(int value) {
-            boolean result = priemTester.isPriemgetal(value);
+        Assertions.assertFalse(result, value + " zou geen priemgetal moeten zijn.");
+    }
 
-            Assertions.assertFalse(result, value + " zou geen priemgetal moeten zijn.");
-        }
+    @ParameterizedTest
+    @ValueSource(ints = {4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20})
+    public void isPriemgetal_onwaar_voor_niet_priemgetallen(int value) {
+        boolean result = priemTester.isPriemgetal(value);
 
-        @ParameterizedTest
-        @ValueSource(ints = {4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20})
-        public void isPriemgetal_onwaar_voor_niet_priemgetallen(int value) {
-            boolean result = priemTester.isPriemgetal(value);
+        Assertions.assertFalse(result, value + " zou geen priemgetal moeten zijn.");
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {2, 3, 5, 7, 11, 13, 17, 19})
+    public void isPriemgetal_waar_voor_priemgetallen(int value) {
+        boolean result = priemTester.isPriemgetal(value);
 
-            Assertions.assertFalse(result, value + " zou geen priemgetal moeten zijn.");
-        }
-        @ParameterizedTest
-        @ValueSource(ints = {2, 3, 5, 7, 11, 13, 17, 19})
-        public void isPriemgetal_waar_voor_priemgetallen(int value) {
-            boolean result = priemTester.isPriemgetal(value);
+        Assertions.assertTrue(result, value + " zou een priemgetal moeten zijn.");
+    }
 
-            Assertions.assertTrue(result, value + " zou een priemgetal moeten zijn.");
-        }
+    @ParameterizedTest
+    @ValueSource(strings = {"101", "103", "1009", "104729", "1299709", "15485863", "32452843"})
+    public void isPriemgetal_waar_voor_BigInteger_priemgetallen(int value) {
+        boolean result = priemTester.isPriemgetal(value);
 
-        @ParameterizedTest
-        @ValueSource(strings = {"101", "103", "1009", "104729", "1299709", "15485863", "32452843"})
-        public void isPriemgetal_waar_voor_BigInteger_priemgetallen(int value) {
-            boolean result = priemTester.isPriemgetal(value);
+        Assertions.assertTrue(result, value + " zou een priemgetal moeten zijn.");
+    }
 
-            Assertions.assertTrue(result, value + " zou een priemgetal moeten zijn.");
-        }
+    @ParameterizedTest
+    @ValueSource(strings = {"100", "102", "1000", "104730", "1299710", "15485864", "32452844"})
+    public void isPriemgetal_onwaar_voor_BigInteger_niet_priemgetallen(int value) {
+        boolean result = priemTester.isPriemgetal(value);
 
-        @ParameterizedTest
-        @ValueSource(strings = {"100", "102", "1000", "104730", "1299710", "15485864", "32452844"})
-        public void isPriemgetal_onwaar_voor_BigInteger_niet_priemgetallen(int value) {
-            boolean result = priemTester.isPriemgetal(value);
-
-            Assertions.assertFalse(result, value + " zou geen priemgetal moeten zijn.");
-        }
+        Assertions.assertFalse(result, value + " zou geen priemgetal moeten zijn.");
     }
 }
